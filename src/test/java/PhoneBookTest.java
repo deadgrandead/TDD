48c9e6698628714ed3a710b0fccbe666d9d495f1
spellbbx <89066808872@mail.ru>
2023-08-16 18:23:54 +0300

    Stub and tests for add

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PhoneBookTest {
+    @Test
+    public void testAdd() {
+        PhoneBook phoneBook = new PhoneBook();
+        assertEquals(1, phoneBook.add("Alice", "12345"));
+        assertEquals(2, phoneBook.add("Bob", "54321"));
+    }
 }
