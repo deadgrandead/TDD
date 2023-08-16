338244c1200d25c56196e31743f718cc44118650
spellbbx <89066808872@mail.ru>
2023-08-16 18:26:40 +0300

    add implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,5 +1,11 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+
+    private Map<String, String> contacts = new HashMap<>();
     public int add(String name, String number) {
-        return 0; // Заглушка
+        contacts.put(name, number);
+        return contacts.size();
     }
 }
